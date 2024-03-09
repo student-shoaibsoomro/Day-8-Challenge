658e8bdf41e431ea231e8e25e017a71e59f192a6
Shoaib Siddiq <159219257+student-shoaibsoomro@users.noreply.github.com>
2024-03-09 08:37:43 +0500

    Add files via upload

---
+++ intentional_error.js
@@ -0,0 +1,6 @@
+"use strict";
+let index = ['Computer', 'Laptop', 'Mobile', 'Ipad'];
+//intentional Error:
+console.log(index[7]);
+//correct the error:
+console.log(index);
---
+++ intentional_error.ts
@@ -0,0 +1,9 @@
+let index: string[] = ['Computer', 'Laptop', 'Mobile', 'Ipad'];
+
+//intentional Error:
+
+console.log(index[7]);
+
+//correct the error:
+
+console.log(index);
