61492a25f82e603cdf75e8a7052585545d1a4691
Shoaib Siddiq <159219257+student-shoaibsoomro@users.noreply.github.com>
2024-03-09 08:39:18 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,26 @@
+"use strict";
+// Equility and Inequility Test
+console.log("Equility test with strings:", "Laptop" == "Laptop");
+console.log("Inequility test with string:", "Laptop" != "Mobile");
+// Test using the lowercase functions:
+console.log('Test with lowercase functions:', "Shoaib Siddiq".toLowerCase() === "shoaib siddiq");
+// Equility test with numericals:
+console.log('Equility test with numbers:', 403 === 403);
+// Inequility test with numericals:
+console.log('Inequility test with numericals:', 403 !== 305);
+// Equility test with greater than:
+console.log('Equility test with greater than:', 100 > 50);
+// Inequility test with less than:
+console.log('Inequility test with less than:', 50 < 100);
+// Greater than or equal to, and less than or equal to:
+console.log('Test with greater than or equal to:', 30 >= 30);
+console.log('Test with less than or equal to:', 20 <= 30);
+// Test using "and" and "or" operators:
+console.log('Test using "and" operators:', 15 === 15 && 3 < 15);
+console.log('Test using "or" operators:', 50 > 40 || 40 > 30);
+// Test whether an item is in a array:
+let Names = ['Ahmed', 'Shoaib', 'Usman', 'Faheem', 'Kabir'];
+console.log('Shoaib name is in the array:', Names.includes('Shoaib'));
+// Test whether an item is not in a array:
+let currency = ['Dollor', 'Riyal', 'Euro', 'Pound'];
+console.log('Dinar is not in the array:', !currency.includes("Dinar"));
