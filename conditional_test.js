859e6e7752113e16857dbd7d4c06bfb6e4018f52
Shoaib Siddiq <159219257+student-shoaibsoomro@users.noreply.github.com>
2024-03-09 08:38:34 +0500

    Add files via upload

---
+++ conditional_test.js
@@ -0,0 +1,4 @@
+"use strict";
+let car = 'Subaru';
+console.log("is car == 'subaru'? I predict True.");
+console.log(car == 'Subaru');
---
+++ main.js
@@ -0,0 +1,58 @@
+"use strict";
+// Task #01
+// Conditional Tests.
+let car = 'Subaru';
+console.log("is car == 'subaru'? I predict True.");
+console.log(car == 'Subaru');
+// Task # 02
+// We are twins Brothers.
+let relative1 = 'love';
+console.log("Is Parents == 'Love'? I predict True");
+console.log(relative1 == 'love');
+// Task # 03
+// Pakistan's Population is 24cr, & India's population is 1.24cr
+let country = 'Pakistan';
+let country1 = 'India';
+console.log(`${country} !== ${country1}'I predict True.`);
+console.log(country !== country1);
+// Task #04
+// Economy of countries.
+let economy1 = 'China';
+let economy2 = 'Pakistan';
+console.log("Pakistan's economy !== 'China's economy'I predict True.");
+console.log(economy1 !== economy2);
+// Task #05
+// Daal chawal & Biriyani:
+let food1 = 'Daal Chawal';
+let food2 = 'Biryani';
+console.log("Daal Chawal !== 'Biryani'I predict True.");
+console.log(food1 !== food2);
+// Task #06
+// Is plan equal to bike?
+let transport = 'plan';
+let transport1 = 'bike';
+console.log("is Plan == 'Bike'I predict False");
+console.log(transport == transport1);
+// Task #07
+// Is 5 thousand equals to 2 thousand?
+let currency = 5000;
+let currency1 = 2000;
+console.log(`${currency} == ${currency1} 'I predict False'`);
+console.log(currency == currency1);
+// Task #08
+// My age and my brothers age.
+let my_Age = 25;
+let brother_Age = 30;
+console.log(`${my_Age} == ${brother_Age} 'I predict False'`);
+console.log(my_Age == brother_Age);
+// Task #09
+// 20 is equal to 15.
+let number1 = 20;
+let number2 = 15;
+console.log(`${number1} == ${number2} 'I predict False'`);
+console.log(number1 == number2);
+// Task #10
+// Iphon is equal to Qmobile.
+let mobile = 'Iphon';
+console.log("is Iphon == 'Qmobile'? I predict False");
+console.log(mobile == 'Qmobile');
